8c6eb5a85e2e1711c62d5c5f687fc79e890e66a4
Rexben <rexben.rb@gmail.com>
2025-08-02 18:29:25 +0200

    chore: support upcoming items

--- src/utils/sort-item.ts
+++ src/utils/sort-item.ts
@@ -1,10 +1,15 @@
 import { Budget } from "../domain/models/budget";
 import { Expense } from "../domain/models/expense";
 
-export function sortItemByRecent<T extends Expense[] | Budget[]>(
-  item: T
+export function sortItemByRecent<T extends (Expense | Budget)[]>(
+  items: T
 ): Array<T[number]> {
-  return [...item].sort((a, b) => {
+  return [...items].sort((a, b) => {
+    // Upcoming items first
+    if (a.upcoming && !b.upcoming) return -1;
+    if (!a.upcoming && b.upcoming) return 1;
+
+    // Then sort by updatedAt (most recent first)
     return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
   });
 }
